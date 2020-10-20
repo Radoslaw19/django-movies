b7729f49164581f444f472edbfa7991758c9d544
Radoslaw <radoslaw.zelazny@interia.pl>
2020-10-20 21:36:37 +0200

    po exit() Nie odpala serwer

--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -14,3 +14,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return f"{self.title} from {self.released}"
+
+# class Genre(models.Model):
+#     name = models.CharField(max_length=20, unique=True)
+#
+#     def __str__(self):
+#         return self.name
--- django_movies/corex/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CorexConfig(AppConfig):
-    name = 'corex'
